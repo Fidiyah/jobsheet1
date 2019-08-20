49f44b84f8106f5f823613988da764cb598f6088
fidiyah <fidiyah_maharani_28rpl@student.smktelkom-mlg.sch.id>
2019-08-20 10:48:19 +0700

    first commit

---
+++ src/praktikum1/DataDiri.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+
+/**
+ *
+ * @fidiyahcantik
+ */
+public class DataDiri {
+  // deklarasi variabel
+   public static void main(String[] args) {
+       // membuat variabel
+       String nama, alamat;
+       int usia;
+       double tinggi;
+       char gender ;
+       String kelas ;
+       String citacita ;
+      double penghasilan ;
+       
+    
+       
+       //mengisi variabel
+       nama = "fidiyah maharani" ;
+       alamat = "dampit" ;
+       usia = 16;
+       tinggi = 160;
+       gender = 'p' ;
+       kelas = "XRPL" ;
+       citacita = "jadi orang cukup,cukup punya 5 apartemen , 10 helikopter,30 lamborghini" ;
+       penghasilan = 10.5 ;
+       
+       
+       
+       // menncetak ke layar isi variabel
+       System.out.println("Nama: " + nama) ;
+       System.out.println("Alamat: " + alamat) ;
+       System.out.println("Usia: " + usia + " tahun") ;
+       System.out.println("Tinggi: " + tinggi + " cm") ;
+       System.out.println("Gender: " + gender) ;
+       System.out.println("Kelas: " + kelas);
+       System.out.println("Cita cita: " + citacita) ;
+       System.out.println("Penghasilan: " + penghasilan + "juta/hari");
+       
+       
+               
+   } 
+}
